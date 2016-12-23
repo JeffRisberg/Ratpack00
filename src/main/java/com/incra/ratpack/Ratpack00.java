646cdb6889d02a0ee3d45ea8f620e8833c9699b5
JeffRisberg <jrisberg@aol.com>
2016-12-23 12:54:28 -0800

    added example of using Guice-based registry of a Service

--- src/main/java/com/incra/ratpack/Ratpack00.java
+++ src/main/java/com/incra/ratpack/Ratpack00.java
@@ -2,6 +2,7 @@ package com.incra.ratpack;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ratpack.guice.Guice;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 
@@ -16,11 +17,20 @@ public class Ratpack00 {
 
         RatpackServer.start(s -> s
                         .serverConfig(config -> config.baseDir(BaseDir.find()))
+
+                        // This is a simple Ratpack-based registration
                         .registryOf(registry -> registry.add(new HelloHandler()))
+
+                        // This is a Guice-based registration
+                        .registry(Guice.registry(r ->
+                                        r.bindInstance(UserService.class, new DefaultUserService())
+                        ))
+
                         .handlers(chain -> chain
                                         .path("foo", ctx -> ctx.render("from the foo handler")) // Map to /foo
                                         .path("bar", ctx -> ctx.render("from the bar handler")) // Map to /bar
                                         .path("hello", ctx -> ctx.get(HelloHandler.class).handle(ctx))
+                                        .path("users", ctx -> ctx.render(ctx.get(UserService.class).getUsers().toString()))
                                         .all(ctx -> ctx.render("root handler!"))
                         )
         );
